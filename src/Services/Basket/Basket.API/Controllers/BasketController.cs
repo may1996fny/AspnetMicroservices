37eddb68631176af626c6317396f708810f919e0
fbihda <fbihda@gmail.com>
2022-04-15 00:12:47 +0200

    Basket API Devloped

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Basket.API.Controllers
+{
+	[ApiController]
+	[Route("api/v1/[controller]")]
+	public class BasketController : ControllerBase
+	{
+		private readonly ILogger<BasketController> logger;
+		private readonly IBasketRepository repository;
+
+		public BasketController(IBasketRepository repository, ILogger<BasketController> logger)
+		{
+			this.repository = repository;
+			this.logger = logger;
+		}
+
+		[HttpGet("{userName}", Name = "GetBasket")]
+		[ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+		public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+		{
+			var basket = await repository.GetBasket(userName);
+			return Ok(basket ?? new ShoppingCart(userName));
+		}
+
+		[HttpPost]
+		[ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+		public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
+		{
+			return await repository.UpdateBasket(basket);
+		}
+
+		[HttpDelete("{userName}", Name = "DeleteBasket")]
+		[ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+		public async Task<IActionResult> DeleteBasket(string userName)
+		{
+			await repository.DeleteBasket(userName);
+			return Ok();
+		}
+	}
+}
---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Basket.API.Entities
+{
+	public class ShoppingCart
+	{
+		public string UserName { get; set; }
+		public List<ShoppingCartItem> Items { get; } = new();
+
+		public ShoppingCart(string userName)
+		{
+			UserName = userName;
+		}
+
+		public ShoppingCart()
+		{
+		}
+
+		public decimal TotalPrice => Items.Sum(i => i.Quantity * i.Price);
+	}
+}
